b3137164047c7040dc581d3ea97a8b7ab968d994
KAGA-KOKO <kotakandis185@gmail.com>
2024-11-15 22:12:42 +0000

    fix derp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,9 +7,8 @@ VENDOR_SECURITY_PATCH := 2020-05-05
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
-# HIDL
+# VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 -include vendor/oppo/CPH2083/BoardConfigVendor.mk
